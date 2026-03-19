305bb13d7f39ca1b72be0988410471ae14686c66
George Hanna <georgeah2005@gmail.com>
2026-03-19 11:21:53 -0700

    Took the values from the dropdown and the input

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const submitButton = document.getElementById("submit")
-const tone = document.getElementById("tone")
-const recipient = document.getElementById("recipient")
+const toneSelect = document.getElementById("tone")
+const recipientSelect = document.getElementById("recipient")
 const input = document.getElementById("emailInput")
 const output = document.getElementById("emailOutput")
 
 submitButton.addEventListener("click", emailGenerator)
 
 function emailGenerator() {
-    
+    console.log("Selected tone: ", toneSelect.value)
+    console.log("Selected recipient: ", recipientSelect.value)
+    console.log("Email Input: ", input.value)
 }
