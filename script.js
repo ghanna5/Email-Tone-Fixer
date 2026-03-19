084adad6c21e578eedcad0316ab14e5e04a9ce27
George Hanna <georgeah2005@gmail.com>
2026-03-19 12:58:57 -0700

    Completed the ai calling using fetch

--- script.js
+++ script.js
@@ -6,8 +6,31 @@ const output = document.getElementById("emailOutput")
 
 submitButton.addEventListener("click", emailGenerator)
 
-function emailGenerator() {
-    const prompt = "Rewrite the following email in a ${toneSelect.value} tone for a ${recipientSelect.value}: ${input.value}"
+async function emailGenerator() {
+    const prompt = `Rewrite the following email in a ${toneSelect.value} tone for a ${recipientSelect.value}: ${input.value}`
 
-    
+    apiURL = "https://api.anthropic.com/v1/messages"
+    const apiKey = "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"
+
+    response = await fetch(apiURL, {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "x-api-key": apiKey,
+            "anthropic-version": "2023-06-01",
+            "anthropic-dangerous-direct-browser-access": "true"
+        },
+        body: JSON.stringify({
+            "messages": [
+                {"role": "user", "content": prompt}
+            ],
+            "model": "claude-sonnet-4-6",
+            "max_tokens": 2000
+        })
+    })
+
+    result = await response.json()
+
+    console.log(result)
+    output.value = result.content[0].text
 }
