d23bc51b3f441b96f5c64cd534aa4483dd735f79
George Hanna <georgeah2005@gmail.com>
2026-03-19 11:07:58 -0700

    Declared all values and got their ids

---
+++ script.js
@@ -0,0 +1,11 @@
+const submitButton = document.getElementById("submit")
+const tone = document.getElementById("tone")
+const recipient = document.getElementById("recipient")
+const input = document.getElementById("emailInput")
+const output = document.getElementById("emailOutput")
+
+submitButton.addEventListener("click", emailGenerator)
+
+function emailGenerator() {
+    
+}
